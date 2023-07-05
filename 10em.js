38bdca4aa7baea219bac4d2309e348980d7a5440
Puroo Kulshrestha <purookulshrestha012@gmail.com>
2023-07-05 11:17:10 +0530

    Add files via upload

---
+++ 10em.js
@@ -0,0 +1,17 @@
+// let ele = document.querySelectorAll('h1');
+// ele.style.color='red'
+// for(let el of ele){
+//     el.style.color='red'
+// }
+
+// console.log(ele.innerhtml)
+// console.log(ele)
+
+
+let ele = document.querySelector('button')
+ele.onclick=function(){
+    console.log('hello')
+}
+// const fun1=()=>{
+//     console.log('hello')
+// }
